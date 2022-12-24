08bc506b9200d6a02e5eaee6030330bd2bcca1c7
khusnidinabdiev <khusnidinabdiev@gmail.com>
2022-12-24 18:43:33 +0500

    50

--- python/1-100/23-chiziqli8.py
+++ python/1-100/23-chiziqli8.py
@@ -4,10 +4,15 @@ a=int(a)
 b=int(b)
 c=int(c)
 d=int(d)
-f=(a*x*x+b*x+c)/(a*a*(a*x+1)+a**(b-c))+math.cos(abs((a*x+b)/(x*c+d+2**c)))
-
-print(format(f, '.2f'))    
-
-
-
-
+if a==0 or x*a+1+pow(a,b-c-2)==0:
+	if c*x+d+pow(2,c)==0:
+		print(format(0, '.2f'))
+	else:
+		f=math.cos(abs((a*x+b)/(x*c+d+pow(2,c))))
+		print(format(f, '.2f'))
+elif c*x+d+pow(2,c)==0:
+	f=(a*x*x+b*x+c)/(a*a*a*x+a*a+pow(a,b-c))
+	print(format(f, '.2f'))
+else:
+	f=(a*x*x+b*x+c)/(a*a*a*x+a*a+pow(a,b-c))+math.cos(abs((a*x+b)/(x*c+d+pow(2,c))))
+	print(format(f, '.2f'))    
---
+++ python/1-100/48-tamoqlanuvchi18.py
@@ -0,0 +1,5 @@
+x=float(input())
+if x<=0:
+	print(format(-x,'.2f'))
+else:
+	print(format(-x*x,'.2f'))
