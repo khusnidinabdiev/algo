2efbd402fe29a3e8046e9067de3c7026af392158
khusnidinabdiev <khusnidinabdiev@gmail.com>
2022-12-28 00:35:16 +0500

    61

--- python/1-100/52-tamoqlanuvchi22.py
+++ python/1-100/52-tamoqlanuvchi22.py
@@ -1,17 +1,17 @@
 x,y=map(float, input().split())
 if y<=1 and y>=-1:
 	if x>=0 and x<=1:
-		if 1.00/3.00*x-1.00/3.00<=y and y<=0:
+		if 1.00/3.00*x-1.00/3.00>=y or y<=0:
 			print('yes')
 		else:
 			print('no')
 	elif x>=-1 and x<0:
-		if 2*x+3>=y or -x>=y:
+		if 2*x+3<=y or -x>=y:
 			print('yes')
 		else:
 			print('no')
 	elif x>=-2 and x<-1:
-		if 2*x+3>=y or 1.00/3.00*x-1.00/3.00<=y:
+		if 2*x+3<=y or 1.00/3.00*x-1.00/3.00>=y:
 			print('yes')
 		else:
 			print('no')
---
+++ python/1-100/59-tamoqlanuvchi29.py
@@ -0,0 +1,5 @@
+x,y=map(float, input().split())
+if 1-abs(x)*2>=y and abs(x)*2-1<=y:
+    print("yes")
+else:
+ 	print("no")
